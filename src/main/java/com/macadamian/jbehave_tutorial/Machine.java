6ccce78331993217b1353747e4473d11909be0ed
Don Kelly <karfai@gmail.com>
2013-09-12 02:38:38 +0400

    fix failures

--- src/main/java/com/macadamian/jbehave_tutorial/Machine.java
+++ src/main/java/com/macadamian/jbehave_tutorial/Machine.java
@@ -3,10 +3,18 @@ package com.macadamian.jbehave_tutorial;
 import java.util.HashMap;
 
 public class Machine {
+    private HashMap<String, Double> _prices = new HashMap<String, Double>();
+
     public Machine() {
+        _prices.put("coke", 1.25);
+        _prices.put("fanta", 0.75);
     }
 
     public Product buy(String name, double amount) {
-        return new Product(name);
+        if (_prices.containsKey(name) && amount >= _prices.get(name)) {
+            return new Product(name);
+        }
+        
+        return null;
     }
 }
